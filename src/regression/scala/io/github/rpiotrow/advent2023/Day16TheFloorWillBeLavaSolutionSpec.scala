c151d8b7f696c4b88de00e112fea78ac9bce3ccb
Rafal Piotrowski <rpiotrow@users.noreply.github.com>
2023-12-17 23:29:44 +0100

    Ignore regression test that last almost 1 minute

--- src/regression/scala/io/github/rpiotrow/advent2023/Day16TheFloorWillBeLavaSolutionSpec.scala
+++ src/regression/scala/io/github/rpiotrow/advent2023/Day16TheFloorWillBeLavaSolutionSpec.scala
@@ -2,6 +2,7 @@ package io.github.rpiotrow.advent2023
 
 import io.github.rpiotrow.advent2023.day16.TheFloorWillBeLava
 import zio.test.Assertion.equalTo
+import zio.test.TestAspect.ignore
 import zio.test.{ZIOSpecDefault, assert}
 
 object Day16TheFloorWillBeLavaSolutionSpec extends ZIOSpecDefault:
@@ -9,4 +10,4 @@ object Day16TheFloorWillBeLavaSolutionSpec extends ZIOSpecDefault:
   def spec = suite("TheFloorWillBeLavaSolutionSpec")(test("The Floor Will Be Lava solution") {
     for solution <- TheFloorWillBeLava.solution
     yield assert(solution)(equalTo((8323L, 8491L)))
-  })
+  }) @@ ignore // this takes too long
